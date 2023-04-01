e99d6f27c73a9d9ab72d648279b7edd0575b717c
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2023-04-02 00:49:34 +0600

    User added complete ( lecture 144 )

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -60,10 +60,10 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="demo" class="collapse">
                             <li>
-                                <a href="#">Item </a>
+                                <a href="users.php">View all Users </a>
                             </li>
                             <li>
-                                <a href="#">item</a>
+                                <a href="users.php?source=add_user">Add User</a>
                             </li>
                         </ul>
                     </li>
